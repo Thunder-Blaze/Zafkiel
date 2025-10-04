2c7aeafa6302f4ceb0e2920b5dddd25ca28ffbc3
ThunderBlaze <darkthunderstormshiva@gmail.com>
2025-10-04 21:07:05 +0530

    Added Providers (tanstack and lenis)

---
+++ src/lib/providers/lenis.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	const { children } = $props();
+	import Lenis from '@studio-freight/lenis';
+
+	$effect(() => {
+		const lenis = new Lenis({
+			smoothWheel: true,
+			lerp: 0.1,
+			orientation: 'vertical',
+			gestureOrientation: 'vertical',
+			syncTouch: false,
+			syncTouchLerp: 0.075,
+			touchInertiaMultiplier: 35,
+			infinite: false,
+			autoResize: true
+		});
+
+		function raf(time: number) {
+			lenis.raf(time);
+			requestAnimationFrame(raf);
+		}
+
+		requestAnimationFrame(raf);
+	});
+</script>
+
+<main>
+	{@render children()}
+</main>
